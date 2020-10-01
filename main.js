dfe2cdc63dd1a56ef03536de2aa3dfb3fa0cf70a
Kirill <skomarovskiykirill@gmail.com>
2020-10-01 14:54:04 +0300

    add continue for loop

--- main.js
+++ main.js
@@ -6,12 +6,13 @@ export const primeNumber = (n) => {
   const arr = Array.from({ length: n }).fill(true, 2);
   
   for (let i = 2; i * i < n; i++) {
-    if (arr[i]) {
-      for (let j = i * i; j < n; j += i) {
-        arr[j] = false;
-      }
+    if (!arr[i]) continue;
+    
+    for (let j = i * i; j < n; j += i) {
+      arr[j] = false;
     }
   }
   
   return arr.reduce((acc, item, i) => item ? acc.concat(i) : acc, []);
 };
+
